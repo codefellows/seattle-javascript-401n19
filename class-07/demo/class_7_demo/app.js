60e75e3e958bf63e91f78cbbaf97098f6191f662
Instructor Alex White <alex.white@codefellows.com>
2021-10-16 13:23:00 -0700

    Class 7 lab

---
+++ class-07/demo/class_7_demo/app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// 3rd party resources
+const express = require('express');
+const morgan = require('morgan');
+const { Sequelize, DataTypes } = require('sequelize');
+const userModel = require('./userModel.js');
+const basicAuth = require('./basic-auth-middleware.js');
+const bearerAuth = require('./bearer-auth-middleware.js');
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/auth_7';
+
+const app = express();
+
+const sequelize = new Sequelize(DATABASE_URL);
+const Users = userModel(sequelize, DataTypes);
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+/* 
+1. First I register 2 new users by POSTing json to /register with Postman
+    a. req.body = { "username":"Zork", "password":"BestBoi" }
+    b. req.body = { "username":"Lemi", "password":"SneakBoi" }
+2. Second, I sign in with the credentials base-64 encoded. I want to verify that I recieve a token back
+    ZORK
+    a. req.headers.authorization = "Basic Wm9yazpCZXN0Qm9p"
+    b. token = 
+    LEMI
+    c. req.headers.authorization = "Basic TGVtaTpTbmVha0JvaQ=="
+    d. token = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkxlbWkiLCJpYXQiOjE2MzQ0MTUyMDZ9.WtNrPJlDs1CQbtzlty9tfmMc_PXTjyDL8TFJQYKWIVY
+3. Thirdly, with the token now in hand, I want to access a route(/user) and authenticate with the token.
+    a. req.headers.authorization = "Bearer ${Lemi token}"
+*/
+
+app.post('/register', (req,res) => {
+    console.log('1. Creating a new User req.body:', req.body,);
+    Users.create(req.body)
+        .then(user => {
+            res.status(200).send(user);
+        })
+        .catch(e => {res.status(403).send('Error creating User')});
+});
+
+app.post('/signin', basicAuth(Users), (req,res) => {
+    console.log('2. I want to sign in with BASIC auth, and get a token back:', req.user, req.user.token);
+    res.status(200).json(req.user);
+});
+
+app.get('/user', bearerAuth(Users), (req,res) => {
+    console.log('3. I am authenticated with my token!', req.user);
+    res.status(200).json('Secret viewing of the saaaaauce');
+});
+
+sequelize.sync()
+    .then(() => {
+        app.listen(3001, () => console.log(`Server up on Port 3001`))
+    })
+    .catch(e => console.error('Could not start sever', e.message))
---
+++ class-07/demo/class_7_demo/userModel.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// We need this secret to "sign" we create with the DataType.Virtual attribute
+// then we verify with verify method
+const SECRET = 'secretSauce';
+
+const userModel = (sequelize, DataTypes) => {
+    const users = sequelize.define('User', {
+      username: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false },
+      token: {
+        type: DataTypes.VIRTUAL,
+        get() {
+          return jwt.sign({ username: this.username }, SECRET);
+        },
+        set(tokenObj) {
+          let token = jwt.sign(tokenObj, SECRET);
+          return token;
+        }
+      }
+    });
+  
+    users.beforeCreate(async (user) => {
+      let hashedPassword = await bcrypt.hash(user.password, 10);
+      user.password = hashedPassword;
+    });
+  
+    users.authenticateToken = async function (token) {
+      try {
+        const parsedToken = jwt.verify(token, SECRET);
+        const user = this.findOne({ username: parsedToken.username })
+        if (user) { return user; }
+        throw new Error("User Not Found");
+      } catch (e) {
+        throw new Error(e.message)
+      }
+    };
+  
+    users.authenticateBasic = async function (username, password) {
+        console.log('User model', username, password);
+      const user = await this.findOne({ where: { username } });
+      const valid = await bcrypt.compare(password, user.password);
+      if (valid) { return user; }
+      throw new Error('Invalid User');
+    }
+  
+    return users;
+  }
+  
+  
+  module.exports = userModel;
